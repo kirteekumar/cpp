214d0480e9e7e40e7d2c6c34a553996080d20e08
kirteekumar <kirti.sonawane999@gmail.com>
2026-03-03 21:28:48 +0100

    update 2

---
+++ day14/day14_4.cpp
@@ -0,0 +1,64 @@
+#include<iostream> 
+
+using namespace std;
+class Base
+{
+    private: 
+    int num1; 
+    int num2; 
+    public: 
+    Base( void )
+    {
+        this->num1 = 0; 
+        this->num2 = 0; 
+    }
+    Base(int num1, int num2)
+    {
+        this->num1 = num1; 
+        this->num2 = num2; 
+    }
+    void showRecord( void )
+    {
+        cout<<"num1 : "<<this->num1<<endl; 
+        cout<<"num2 : "<<this->num2<<endl; 
+    }
+    void printRecord( void )
+    {
+        cout<<"num1 : "<<this->num1<<endl; 
+        cout<<"num2 : "<<this->num2<<endl; 
+    }
+};
+class Derived : public Base
+{
+    private: 
+    int num3; 
+    public: 
+    Derived( void )
+    {
+        this->num3 = 0; 
+    }
+    Derived(int num1 , int num2 , int num3) : Base(num1,num2)
+    {
+        this->num3 = num3; 
+    }
+    void displayRecord( void )
+    {
+        Base::showRecord( ); 
+        cout<<"num3 : "<<this->num3<<endl; 
+    }
+    void printRecord( void )
+    {
+        Base::printRecord( );
+        cout<<"num3 : "<<this->num3<<endl;   
+    }
+}; 
+
+int main()
+{
+    Base b1(10,20);
+    Base b2;
+    b2=b1;
+    b2.printRecord();
+
+    return 0;
+}
