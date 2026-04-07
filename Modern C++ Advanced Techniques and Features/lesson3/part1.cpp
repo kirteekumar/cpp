3037174b1a39d9109a6ecd2f296e7a189678290e
kirteekumar <kirti.sonawane999@gmail.com>
2026-04-07 21:21:18 +0200

    update

--- Modern C++ Advanced Techniques and Features/lesson3/part1.cpp
+++ Modern C++ Advanced Techniques and Features/lesson3/part1.cpp
@@ -1,35 +1,32 @@
 #include <iostream>
 #include <vector>
+#include <initializer_list>
+
 
 using namespace std;
 
-//xvalue
-//temp lifespan
+std::vector<int>  v={100, 200, 300};
 
-
-//&& - rvalue reference
-
-class Out
+void f1(std::initializer_list<int> nums)
 {
-
-};
-
-Out calculate()
-{
-	Out o;
-	return o;
+	for(auto n:nums)
+		std::cout<<n<<std::endl;
 }
 
-void someFunc(Out &&o_rv)
-{
+auto i1 = 10; 	      //int
+auto i2(10);	      //int
+auto i3{10};	      //int
+auto i4={10};	      //std::initializer_list
 
-}
+std::vector<int> vec1(i1); //10 items
+std::vector<int> vec2(i2); //10 items
+std::vector<int> vec3(i3); //10 items
+std::vector<int> vec4(i4); //1 item
 
 
 int main()
 {
-	someFunc(calculate()); //OK
-
+	f1({10,20,30});
 	return 0;
 
 }
