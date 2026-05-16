4827a5b292ecf066ef6b11c1f9d15596ae7775cb
kirteekumar <kirti.sonawane999@gmail.com>
2026-05-16 12:09:24 +0200

    update

--- ProgrammingPrinciplesAndPracticeUsingQt/Chapter09/Practice_09_05_01.cpp
+++ ProgrammingPrinciplesAndPracticeUsingQt/Chapter09/Practice_09_05_01.cpp
@@ -1,12 +1,22 @@
-
-#include <string>
-#include <functional>
+#include <iostream>
 
 using namespace std;
 
-int main(){
+int main()
+{
+    std::cout<<"Hello World";
+    int n; 
+    
+    while(true)
+    {
+        cin>>n;
+        if(cin)
+        {
+            if(1<=n && n<=10)
+                return n;
+            cout << "Sorry " << n << "is not in the range" << endl;
+        }
+    }
 
-	return 0;
+    return 0;
 }
-
-
