c4eff7f7f30dfdb7ed2e439b7b5c285dab890600
kirteekumar <kirti.sonawane999@gmail.com>
2026-05-17 19:13:13 +0200

    update

--- ProgrammingPrinciplesAndPracticeUsingQt/Chapter09/Drill_01.cpp
+++ ProgrammingPrinciplesAndPracticeUsingQt/Chapter09/Drill_01.cpp
@@ -1,11 +1,18 @@
 
+/*
+[1]
+Start a program called Test_output.cpp. Declare an integer birth_year and assign it the year
+you were born.
+*/
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    int n; 
+    int birth_year = 1988; 
     
     return 0;
 }
+
