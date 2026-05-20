85b71ec44a98c66bee3872fb76ddac86c27f29e8
kirteekumar <kirti.sonawane999@gmail.com>
2026-05-20 23:17:21 +0200

    Chapter 17 practice

--- ProgrammingPrinciplesAndPracticeUsingQt/Chapter17/Practice_17_2.cpp
+++ ProgrammingPrinciplesAndPracticeUsingQt/Chapter17/Practice_17_2.cpp
@@ -2,8 +2,21 @@
 
 using namespace std;
 
+
+class Vector {
+		int sz;
+		double *elem;
+		
+		public:
+		double operator [](int n) {return elem[n];}
+		Vector(int n) {sz = n; elem = new double[n];}
+};
+
 int main()
 {
-
+    Vector v(9);
+    double x = v[0];
+    
+    cout << "x : " << x << endl ;
 	return 0;
 }
