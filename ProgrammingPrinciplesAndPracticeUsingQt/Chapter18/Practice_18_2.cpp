9384664aab20d65bcb635c2af11455ec60929615
kirteekumar <kirti.sonawane999@gmail.com>
2026-05-26 21:12:11 +0200

    update

--- ProgrammingPrinciplesAndPracticeUsingQt/Chapter18/Practice_18_2.cpp
+++ ProgrammingPrinciplesAndPracticeUsingQt/Chapter18/Practice_18_2.cpp
@@ -11,25 +11,33 @@ class Vector {
 	int space;
 	// size + free space
 	public:
-	Vector() :sz{0}, elem{nullptr}, space{0} { }
+	Vector() :sz{0}, elem{nullptr}, space{0} { 
+	    cout<<"default : "<<endl;
+	}
+	
 	explicit Vector(int s) :sz{s}, elem{new T[s]}, space{s}
 	{
+	    cout<<"explicit with argument : "<<s<<" "<<endl;
 		for (int i=0; i<sz; ++i)
-		elem[i]=0;
+		    elem[i]=0;
 		// elements are initialized
 	}
+	
 	Vector(initializer_list<T>);
 	// list constructor
 	Vector& operator=(initializer_list<T>);
 	// list assignment
+	
 	Vector(const Vector&);
 	// copy constr uctor
 	Vector& operator=(const Vector&);
 	// copy assignment
+	
 	Vector(Vector&& );
 	// move constr uctor
 	Vector& operator=(Vector&&);
 	// move assignment
+	
 	~Vector() { delete[] elem; }
 	// destr uctor
 	T& operator[](int n) 
@@ -44,7 +52,7 @@ class Vector {
 	// the current size
 	int capacity() const { return space; }
 	// the current capacity
-	void resize(int newsize);
+	void resize(int newsize, T def= T{});
 	// growth
 	void push_back(const T& d);
 	void reserve(int newalloc);
@@ -53,9 +61,33 @@ class Vector {
 	T* end() const { return elem+sz; }
 };
 
+template<typename T>
+void Vector <T>::resize(int newsize, T d)
+{
+    
+    
+}
+
+class No_default
+{
+    public:
+    No_default(){
+        cout<<"No_default default : "<<endl;
+    }
+    No_default(int a){
+        cout<<"No_default with arg : "<<a<<" "<<endl;
+    }
+};
+
 
 int main()
 {
+    Vector<double>  v;
+    Vector<double>  v1(5);
+    Vector<char>    v2;
+	
+	Vector<No_default> nd1;
+	Vector<No_default> nd2(1);
 	
 	return 0;
 }
