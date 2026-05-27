ef4dab147d84ec212cdb3a4e6a129b7a2e509dec
kirteekumar <kirti.sonawane999@gmail.com>
2026-05-27 21:13:49 +0200

    update

--- ProgrammingPrinciplesAndPracticeUsingQt/Chapter18/Practice_18_4_4.cpp
+++ ProgrammingPrinciplesAndPracticeUsingQt/Chapter18/Practice_18_4_4.cpp
@@ -2,7 +2,8 @@
 #include <memory>
 
 template<typename T, typename A = std::allocator<T>>
-struct Vector_rep {
+struct Vector_rep 
+{
     A alloc; // allocator
     int sz; // number of elements
     T* elem; // star t of allocation
@@ -12,11 +13,36 @@ struct Vector_rep {
     ~Vector_rep() { alloc.deallocate(elem, space); }
 };
 
+template<typename T, typename A = std::allocator<T>>
+class Vector 
+{
+	Vector_rep<T,A> r;
+	public:
+	Vector() : r{A{} ,0} { }
+	explicit Vector(int s) :r{A{},s}
+	{
+		for (int i = 0; i < r.sz; ++i)
+			r.elem[i] = 0; 	// elements are initialized
+	}
+	
+	void reserve(int newalloc);
+};
+
+
+template<typename T, typename A>
+void Vector<T,A>::reserve(int newalloc)
+{
+    if (newalloc <= r.space) // never decrease allocation
+        return;
+    Vector_rep<T, A> b{ r.alloc ,newalloc }; // allocate new space
+    std::uninitialized_move(r.elem, r.elem+r.sz, b.elem); // move
+    std::destroy(r.elem, r.elem + r.sz); // destroy the old elements
+    std::swap(r, b); // swap representations
+}
+
+
 int main()
 {
-    std::allocator allo;
-    
-    Vector_rep(allo,1);
 	
 	return 0;
 }
