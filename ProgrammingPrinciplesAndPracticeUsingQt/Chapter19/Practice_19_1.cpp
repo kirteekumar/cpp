68f57e0ad02f4a5492671a020a2cb1efedd1a9ab
kirteekumar <kirti.sonawane999@gmail.com>
2026-05-29 12:11:55 +0200

    update

--- ProgrammingPrinciplesAndPracticeUsingQt/Chapter19/Practice_19_1.cpp
+++ ProgrammingPrinciplesAndPracticeUsingQt/Chapter19/Practice_19_1.cpp
@@ -1,4 +1,23 @@
+#include "iostream"
+#include <vector>
+
+double * get_from_jack(int * count){ return 0;}
+std::vector<double> get_from_jill(){ return std::vector<double>(1); }
+
+void fct()
+{
+	int jack_count = 0;
+	double * jack_data = get_from_jack(&jack_count);
+	std::vector<double> jill_data = get_from_jill();
+
+	//process
+	delete[] jack_data;
+
+}
+
 int main()
 {
+	fct();
+	
 	return 0;
 }
