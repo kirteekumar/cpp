9f92a3f5136b6762b41aa0ea987b79eaeff6b6d0
kirteekumar <kirti.sonawane999@gmail.com>
2026-05-30 21:19:32 +0200

    update

--- ProgrammingPrinciplesAndPracticeUsingQt/Chapter19/Practice_19_4.cpp
+++ ProgrammingPrinciplesAndPracticeUsingQt/Chapter19/Practice_19_4.cpp
@@ -4,11 +4,26 @@
 template<typename T, typename A = std::allocator<T>>
 class Vector {
 	public:
+		using size_type=int;
+		using value_type=T;
+		using iterator = T*;
+		using const_iterator = const T*;
+
+
+		iterator begin();
+		const_iterator begin() const;
+		iterator end();
+		const_iterator end() const;
+
+		size_type size();
 };
 
 
 int main()
 {
+	Vector<int> v;
+	//Vector<int>::iterator p = find(v.begin(), v.end(), 32);
+
 	return 0;
 }
 
