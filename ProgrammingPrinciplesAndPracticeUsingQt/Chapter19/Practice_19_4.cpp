e00137bc8a6eadfbccb611758c9b51abab2d6a97
kirteekumar <kirti.sonawane999@gmail.com>
2026-05-30 21:39:10 +0200

    update

--- ProgrammingPrinciplesAndPracticeUsingQt/Chapter19/Practice_19_4.cpp
+++ ProgrammingPrinciplesAndPracticeUsingQt/Chapter19/Practice_19_4.cpp
@@ -16,6 +16,26 @@ class Vector {
 		const_iterator end() const;
 
 		size_type size();
+
+		T operator[](T){ return T{}; }
+};
+
+template <typename T, typename A=std::allocator<T>>
+class List {
+	public:
+		using size_type = int;
+		using value_type = T;
+		class Link;
+		class iterator;
+		class const_iterator;
+
+		iterator begin();
+		const_iterator begin() const;
+		iterator end();
+		const_iterator end() const;
+
+		size_type size();
+		//...
 };
 
 
@@ -23,6 +43,11 @@ int main()
 {
 	Vector<int> v;
 	//Vector<int>::iterator p = find(v.begin(), v.end(), 32);
+
+	for(Vector<int>::size_type i=0;i<v.size();++i)
+		std::cout<<v[i]<<'\n';
+
+	//19.4.1 : container traversal:
 
 	return 0;
 }
